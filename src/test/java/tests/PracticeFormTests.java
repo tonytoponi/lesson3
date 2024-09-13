b030ff81b459806661b72f2d786b858f83a208b0
Anton Zhilkin <TonyZhilkin@gmail.com>
2024-09-14 01:03:42 +0300

    Updated according review, changed locating method for byText searches, added ad and footer remover. Changed assertions

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -8,6 +8,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
 
 public class PracticeFormTests {
     @BeforeAll
@@ -22,42 +23,43 @@ public class PracticeFormTests {
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
         String firstName = "Tony";
         String lastName = "Soprano";
         String userEmail = "test@test.com";
         String phone = "1234567890";
         String gender = "Male";
-        String outputForm = ".table-responsive";
 
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(userEmail);
-        $(byText(gender)).click();
+        $("#genterWrapper").$(byText(gender)).click();
         $("#userNumber").setValue(phone);
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").find("option[value='1']").click();
         $(".react-datepicker__year-select").find("option[value='1991']").click();
         $(".react-datepicker__day--014").click();
         $("#subjectsInput").setValue("Accounting").pressEnter();
-        $(byText("Sports")).click();
-        $(byText("Reading")).click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#uploadPicture").uploadFromClasspath("samia.jpg");
         $("#currentAddress").setValue("Some street 1");
         $("#state").click();
-        $(byText("NCR")).click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
-        $(byText("Delhi")).click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
 
-        $(outputForm).shouldHave(text("Tony Soprano"));
-        $(outputForm).shouldHave(text("test@test.com"));
-        $(outputForm).shouldHave(text("Male"));
-        $(outputForm).shouldHave(text("1234567890"));
-        $(outputForm).shouldHave(text("14 February,1991"));
-        $(outputForm).shouldHave(text("Accounting"));
-        $(outputForm).shouldHave(text("Sports, Reading"));
-        $(outputForm).shouldHave(text("samia.jpg"));
-        $(outputForm).shouldHave(text("Some street 1"));
-        $(outputForm).shouldHave(text("NCR Delhi"));
+        $(".table-responsive").shouldHave(text("Tony Soprano"));
+        $(".table-responsive").shouldHave(text("test@test.com"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("1234567890"));
+        $(".table-responsive").shouldHave(text("14 February,1991"));
+        $(".table-responsive").shouldHave(text("Accounting"));
+        $(".table-responsive").shouldHave(text("Sports, Reading"));
+        $(".table-responsive").shouldHave(text("samia.jpg"));
+        $(".table-responsive").shouldHave(text("Some street 1"));
+        $(".table-responsive").shouldHave(text("NCR Delhi"));
     }
 }
